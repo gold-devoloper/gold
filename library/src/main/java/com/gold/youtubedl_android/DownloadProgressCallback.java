7cfce0cafce6296bb192fa5de2b1baad8457117f
Lenovo <melizyayevx@gmail.com>
2022-12-26 11:31:19 +0500

    finish

---
+++ library/src/main/java/com/gold/youtubedl_android/DownloadProgressCallback.java
@@ -0,0 +1,5 @@
+package com.gold.youtubedl_android;
+
+public interface DownloadProgressCallback {
+    void onProgressUpdate(float progress, long etaInSeconds, String line);
+}
---
+++ library/src/main/java/com/gold/youtubedl_android/mapper/VideoSubtitle.java
@@ -0,0 +1,8 @@
+package com.gold.youtubedl_android.mapper;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VideoSubtitle {
+
+}
